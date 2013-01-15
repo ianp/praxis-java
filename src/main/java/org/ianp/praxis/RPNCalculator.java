3528248c0f81ccb1b5202c7604e834f8a0b4ea3e
Ian Phillips <ianp@ianp.org>
2013-01-15 14:16:41 +0100

    Added RPN calculator.

---
+++ src/main/java/org/ianp/praxis/RPNCalculator.java
@@ -0,0 +1,55 @@
+package org.ianp.praxis;
+
+import java.io.Console;
+import java.math.BigDecimal;
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+/**
+ * A simple reverse-polish notation calculator.
+ * <p>
+ * A solution to <a href="http://programmingpraxis.com/2009/02/19/rpn-calculator/">this problem</a>.
+ */
+public class RPNCalculator {
+
+    private static final Console console = System.console();
+    private static final Deque<BigDecimal> stack = new ArrayDeque<>();
+   
+    private static void processLine(String line) {
+        for (String s : line.split("\\s+")) {
+            switch (s) {
+            case "+":
+                stack.push(stack.pop().add(stack.pop()));
+                break;
+            case "-":
+                stack.push(stack.pop().subtract(stack.pop()));
+                break;
+            case "*":
+                stack.push(stack.pop().multiply(stack.pop()));
+                break;
+            case "/":
+                stack.push(stack.pop().divide(stack.pop()));
+                break;
+            case "quit":
+            case "exit":
+                System.exit(0);
+            default:
+                stack.push(new BigDecimal(s));
+            }
+        }
+        console.format("%s%n", stack.peek()).flush();
+    }
+
+    public static void main(String[] args) {
+        try {
+            String line;
+            while ((line = console.readLine("> ")) != null) {
+                processLine(line);
+            }
+        } catch (Exception e) {
+            console.format("%s: %s%n", e.getClass().getSimpleName(), e.getMessage());
+        }
+    }
+
+}
+
